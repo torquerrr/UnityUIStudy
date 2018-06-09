7187758f9eaa0b0dc36744aabc47ced0a77b068e
Victor Tarnovskiy <float34@gmail.com>
2018-06-09 21:42:19 +0300

    Refactoring

--- Assets/Scripts/WindowManager.cs
+++ Assets/Scripts/WindowManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class WindowManager : MonoBehaviour {
 
     public static WindowManager instance = null;
+    private class SwitchEvent : UnityEvent<bool> { };
 
     [Header("Available windows")]
     [SerializeField]
@@ -14,18 +16,35 @@ public class WindowManager : MonoBehaviour {
     [SerializeField]
     private GameObject containersPanel;
 
-    public bool NeedToSwitch { get; set; }
-    public Windows Window { get; set; }
+    private GameObject mainMenuClone;
+    private GameObject modalPanelClone;
+    private GameObject containersPanelClone;
+    private SwitchEvent onNeedToSwitch;
+    private bool needToSwitch;
+    private Windows _Window;
+
+    public Windows Window
+    {
+        get
+        {
+            return _Window;
+        }
+        set
+        {
+            _Window = value;
+            if (onNeedToSwitch != null)
+            {
+                onNeedToSwitch.Invoke(true);
+            }
+        }
+    }
+
     public enum Windows
     {
         MainMenu,
         ModalPanel,
         ContainersPanel
     }
-
-    private GameObject mainMenuClone;
-    private GameObject modalPanelClone;
-    private GameObject containersPanelClone;
 
     private void Awake()
     {
@@ -37,14 +56,18 @@ public class WindowManager : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+
+        // event handler for Window value change
+        onNeedToSwitch = new SwitchEvent();
+        onNeedToSwitch.AddListener((switchKey) => needToSwitch = switchKey);
+
         // set default window
         Window = Windows.MainMenu;
-        NeedToSwitch = true;
     }
 
     private void Update()
     {
-        if (NeedToSwitch)
+        if (needToSwitch)
         {
             switch (Window)
             {
@@ -52,19 +75,19 @@ public class WindowManager : MonoBehaviour {
                     mainMenuClone = Instantiate(mainMenu, this.transform);
                     Destroy(modalPanelClone);
                     Destroy(containersPanelClone);
-                    NeedToSwitch = false;
+                    onNeedToSwitch.Invoke(false);
                     break;
                 case Windows.ModalPanel:
                     modalPanelClone = Instantiate(modalPanel, this.transform);
                     Destroy(mainMenuClone);
                     Destroy(containersPanelClone);
-                    NeedToSwitch = false;
+                    onNeedToSwitch.Invoke(false);
                     break;
                 case Windows.ContainersPanel:
                     containersPanelClone = Instantiate(containersPanel, this.transform);
                     Destroy(modalPanelClone);
                     Destroy(mainMenuClone);
-                    NeedToSwitch = false;
+                    onNeedToSwitch.Invoke(false);
                     break;
             }
         }
--- Assets/Scripts/WindowSwitcher.cs
+++ Assets/Scripts/WindowSwitcher.cs
@@ -1,13 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class WindowSwitcher : MonoBehaviour {
 
 	public void ChangeWindow(int winId)
     {
         WindowManager.instance.Window = (WindowManager.Windows)winId;
-        WindowManager.instance.NeedToSwitch = true;
 	}
 }
