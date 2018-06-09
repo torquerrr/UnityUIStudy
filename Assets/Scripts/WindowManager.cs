74dbef542f74d896e49c6bc28f4f32b765809ed9
Victor Tarnovskiy <float34@gmail.com>
2018-06-09 11:15:25 +0300

    Implemented Window Manager

---
+++ Assets/Scripts/WindowManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowManager : MonoBehaviour {
+
+    public static WindowManager instance = null;
+
+    [Header("Available windows")]
+    [SerializeField]
+    private GameObject mainMenu;
+    [SerializeField]
+    private GameObject modalPanel;
+    [SerializeField]
+    private GameObject containersPanel;
+
+    public bool NeedToSwitch { get; set; }
+    public Windows Window { get; set; }
+    public enum Windows
+    {
+        MainMenu,
+        ModalPanel,
+        ContainersPanel
+    }
+
+    private GameObject mainMenuClone;
+    private GameObject modalPanelClone;
+    private GameObject containersPanelClone;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        // set default window
+        Window = Windows.MainMenu;
+        NeedToSwitch = true;
+    }
+
+    private void Update()
+    {
+        if (NeedToSwitch)
+        {
+            switch (Window)
+            {
+                case Windows.MainMenu:
+                    mainMenuClone = Instantiate(mainMenu, this.transform);
+                    Destroy(modalPanelClone);
+                    Destroy(containersPanelClone);
+                    NeedToSwitch = false;
+                    break;
+                case Windows.ModalPanel:
+                    modalPanelClone = Instantiate(modalPanel, this.transform);
+                    Destroy(mainMenuClone);
+                    Destroy(containersPanelClone);
+                    NeedToSwitch = false;
+                    break;
+                case Windows.ContainersPanel:
+                    containersPanelClone = Instantiate(containersPanel, this.transform);
+                    Destroy(modalPanelClone);
+                    Destroy(mainMenuClone);
+                    NeedToSwitch = false;
+                    break;
+            }
+        }
+    }
+
+    public Windows GetCurrentWindow()
+    {
+        Debug.Log($"Current window is {Window}");
+        return Window;
+    }
+}
---
+++ Assets/Scripts/WindowSwitcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowSwitcher : MonoBehaviour {
+
+	public void ChangeWindow(int winId)
+    {
+        WindowManager.instance.Window = (WindowManager.Windows)winId;
+        WindowManager.instance.NeedToSwitch = true;
+	}
+}
